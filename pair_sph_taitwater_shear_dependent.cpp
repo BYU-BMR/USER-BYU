e57b8e4a9570db0478df0ed970295bf825bf5295
Anthony Gillespie <agilles2@m8int01.fsl.byu.edu>
2017-05-15 10:11:18 -0600

    Now compatible with new error location propagtion introduced in lammps commit 35753b8f089e1f61301f69a58c2452fe908cf904

--- pair_sph_taitwater_shear_dependent.cpp
+++ pair_sph_taitwater_shear_dependent.cpp
@@ -4,7 +4,7 @@
  Steve Plimpton, sjplimp@sandia.gov
 
  Copyright (2003) Sandia Corporation.  Under the terms of Contract
- DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+ DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retainsforce
  certain rights in this software.  This software is distributed under
  the GNU General Public License.
 
@@ -248,8 +248,8 @@ void PairSPHTaitwaterShearDependent::coeff(int narg, char **arg) {
     allocate();
 
   int ilo, ihi, jlo, jhi;
-  force->bounds(arg[0], atom->ntypes, ilo, ihi);
-  force->bounds(arg[1], atom->ntypes, jlo, jhi);
+  force->bounds(FLERR,arg[0], atom->ntypes, ilo, ihi);
+  force->bounds(FLERR,arg[1], atom->ntypes, jlo, jhi);
 
   double rho0_one = force->numeric(FLERR,arg[2]);
   double soundspeed_one = force->numeric(FLERR,arg[3]);
